bf26f07aa61683a526c8924057f58ef787aef71b
sara-attarzadeh <s.attarzadeh94@gmail.com>
2019-04-20 22:44:58 -0400

    Add event listener to handle article clicks, show popup, and add pop up content. Add loading icon display.

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@
 
 //article dom elements
 const mainFeed = document.querySelector('#main');
-const article = document.querySelector('.article');
+const articles = document.querySelectorAll('.article');
 const featuredImage = document.querySelector('.featuredImage img');
 const articleLink = document.querySelector('.articleContent a');
 const articleTitle = document.querySelector('.articleContent h3');
@@ -30,6 +30,10 @@ const newsSourceLists = document.querySelectorAll('nav ul li ul li');
 const searchBar = document.querySelector('#search');
 const searchInput = document.querySelector('input');
 
+//popup dom elements
+const popup = document.querySelector('#popUp');
+const popupX = document.querySelector('.closePopUp');
+
 const xhr = new XMLHttpRequest();
 const API_KEY = 'bdaee733f58d4d8e8716f97c8b30fb29';
 const BASE_URL = `https://newsapi.org/v2/everything?apiKey=${API_KEY}&q=`
@@ -37,15 +41,33 @@ const BASE_URL = `https://newsapi.org/v2/everything?apiKey=${API_KEY}&q=`
 
 //event listeners
 //dropdown clicks
+document.addEventListener('DOMContentLoaded', function(){
+  mainFeed.innerHTML = "";
+  getNews("video game OR gaming&language=en");
+});
+
 newsSourceDropDown[0].addEventListener('click', function(){
+  mainFeed.innerHTML = "";
   currentNewsSource.innerText = newsSourceDropDown[0].innerText;
   getNews("video game OR gaming&language=en");
 });
 
 newsSourceDropDown[1].addEventListener('click', function(){
+  mainFeed.innerHTML = "";
   currentNewsSource.innerText = newsSourceDropDown[1].innerText;
   getNews('"Tom Hanks"&language=en');
 });
+
+newsSourceDropDown[2].addEventListener('click', function(){
+  mainFeed.innerHTML = "";
+  currentNewsSource.innerText = newsSourceDropDown[2].innerText;
+  //entertainment-weekly, contains words = "dies"
+  getNews('"baby goat"&language=en');
+});
+
+popupX.addEventListener('click', function(){
+  popup.classList.add('hidden');
+})
 
 
 //necessary fields: article image (urlToImage), article title(title), article author (author), article date(publishedAt)
@@ -60,6 +82,9 @@ function getNews(searchTerm) {
 function onSuccess() {
   console.log(xhr.status);
   if(xhr.status >= 200 && xhr.status < 300) {
+    popup.classList.add('loader');
+    popup.classList.remove('hidden');
+    runLoadingIcon();
     const results = JSON.parse(xhr.responseText);
     const articleArray = results.articles;
     articleArray.forEach(function(currentVal){
@@ -86,6 +111,24 @@ function onSuccess() {
       mainFeed.appendChild(htmlArticleSegment);
     });
     console.log(results);
+
+    mainFeed.addEventListener('click', function(event){
+      if(event.target && event.target.closest('.articleContent')) {
+       // const element = event.target;
+       // console.log(element);
+       const childArticleNode = event.target.closest('article');
+       const parentNode = childArticleNode.parentNode.parentNode;
+       console.log(childArticleNode);
+       console.log(parentNode);
+       const indexOfClickedArticle = Array.prototype.indexOf.call(parentNode.children, childArticleNode.parentNode);
+       //make popup visible and add relevant Content
+       popup.classList.remove('hidden', 'loader');
+       popup.querySelector('h1').innerText = articleArray[indexOfClickedArticle].title;
+       popup.querySelector('p').innerText = articleArray[indexOfClickedArticle].description;
+       popup.querySelector('.popUpAction').href = articleArray[indexOfClickedArticle].url;
+
+      }
+    }, false);
   } else {
     console.log('oops, we have a' + xhr.status + ". Please contact no one");
   }
@@ -114,4 +157,13 @@ String.prototype.replaceAt=function(index, replacement, originalWord) {
     const stringCharDifference = originalWord.length - replacement.length;
     console.log(stringCharDifference);
     return this.substr(0, index) + replacement + (this.substr(index + replacement.length + stringCharDifference));
+}
+
+//hide loading icon
+function runLoadingIcon() {
+  setTimeout(function(){
+    popup.classList.remove('loader');
+    popup.classList.add('hidden');
+  }, 800);
+  // popup.classList.remove('loader');
 }
